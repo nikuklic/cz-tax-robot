f6ce723091b143810ab139ea0ae7ff5edf8a3f12
Denis Postu <depostu@microsoft.com>
2019-07-22 18:23:00 +0200

    Added ability to parse line by line entries in Fidelity reports

--- fidelityMonthlyReportsParser.js
+++ fidelityMonthlyReportsParser.js
@@ -4,36 +4,76 @@ const fs = require('fs');
 const { getLinesFromPdfAsync } = require('./utils/pdf');
 
 const extractMeaningfulInformation = fidelityReportLines => {
-    const nthValueAfter = (n) => (field, section) => {
-        const subArray = section ? fidelityReportLines.slice(fidelityReportLines.indexOf(section)) : fidelityReportLines;
-        return subArray[subArray.indexOf(field) + n];
-    };
-    const firstValueAfter = nthValueAfter(1);
-    const secondValueAfter = nthValueAfter(2);
-
-    const firstNumberAfter = (field, section) => tofloat(firstValueAfter(field, section)) || 0;
-    const secondNumberAfter = (field, section) => tofloat(secondValueAfter(field, section)) || 0;
-
     const tofloat = value => parseFloat(value.replace('$', '').replace(',', ''));
     const negate = number => typeof number === 'number' ? -number : number;
-        
-    const esppIncome = negate(secondNumberAfter('Total Securities Bought'));
-    const specialStockVests = firstNumberAfter('Securities Transferred In');
-    const regularStockVests = firstNumberAfter('Other Activity In', 'Core Account and Credit Balance Cash Flow') - esppIncome;
+
+    const getLocation = (field, section, delta = 0) => {
+        const offset = section 
+            ? fidelityReportLines.indexOf(section)
+            : delta || 0;
+
+        const subArray = fidelityReportLines.slice(offset);
+        const subArrayIndex = subArray.indexOf(field);
+
+        if (delta && subArrayIndex < 0) {
+            return undefined;
+        }
+
+        return {
+            next: () => getLocation(field, undefined, subArrayIndex + offset + 1),
+            nextString: n => fidelityReportLines[offset + subArrayIndex + n],
+            nextFloat: (n = 1) => tofloat(fidelityReportLines[offset + subArrayIndex + n]) || 0,
+        };
+    }
+            
+    const esppIncome = negate(getLocation('Total Securities Bought').nextFloat(2));
+    const specialStockVests = getLocation('Securities Transferred In').nextFloat(1);
+    const regularStockVests = getLocation('Other Activity In', 'Core Account and Credit Balance Cash Flow').nextFloat() - esppIncome;
     
-    return {
+    const reportSummary = {
         period: fidelityReportLines[1],
         stocks: {
             received: specialStockVests + regularStockVests
         },
         dividends: {
-            received: firstNumberAfter('Total Dividends, Interest & Other Income'),
-            taxesPaid: negate(firstNumberAfter('Total Federal Taxes Withheld'))
+            received: getLocation('Total Dividends, Interest & Other Income').nextFloat(),
+            taxesPaid: negate(getLocation('Total Federal Taxes Withheld').nextFloat())
         },
         espp: {
             bought: esppIncome
         }
     }
+
+    if (esppIncome) {
+        const { nextString, nextFloat } = getLocation('MICROSOFT CORP ESPP###', 'Securities Bought & Sold');
+
+        reportSummary.espp.list = [{ 
+            date: nextString(-1).trim(),
+            symbol: nextString(1).trim(),
+            quantity: nextFloat(3),
+            price: nextFloat(4), 
+            amount: negate(nextFloat(6))
+        }];
+    }
+    
+    if (specialStockVests || regularStockVests) {
+        let location;
+
+        reportSummary.stocks.list = [];
+        
+        while (location = location ? location.next() : getLocation('MICROSOFT CORP SHARES DEPOSITED')) {
+            const { nextString, nextFloat } = location;
+            reportSummary.stocks.list.push({
+                date: nextString(-1).trim(),
+                symbol: nextString(2).trim(),
+                quantity: nextFloat(4),
+                price: nextFloat(5),
+                amount: tofloat((nextFloat(4) * nextFloat(5)).toFixed(2)),
+            })
+        }
+    }
+
+    return reportSummary;
 }
 
 function parseFidelityReports(absolutePathToReportsDirectory) {
