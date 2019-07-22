f2c84ce1139e3f20a4e4c30d2fb704be1bc48f6b
Adrian Bosilca <adbosi@microsoft.com>
2019-07-22 18:25:31 +0200

    Fully working morgan stanley parser without conversion

--- morganStanleyParser.js
+++ morganStanleyParser.js
@@ -1,13 +1,13 @@
-var pdfreader = require("pdfreader");
-var fs = require("fs");
+const path = require('path');
+const pdfreader = require("pdfreader");
+const fs = require("fs");
 
-const filename = "C:\\Users\\adbosi\\Downloads\\tax documents 2018 final\\tax documents 2018\\morgan-q1.pdf";
 const alignmentTolerance = 3;
 
-function getTable() {
+function getTable(filePath) {
     return new Promise((resolve, reject) => {
         let isParsing = false;
-        fs.readFile(filename, (err, pdfBuffer) => {
+        fs.readFile(filePath, (err, pdfBuffer) => {
             // pdfBuffer contains the file content
             new pdfreader.PdfReader().parseBuffer(pdfBuffer, function(err, item) {
                 if (err) {
@@ -63,15 +63,33 @@ function extractTransactions(table) {
     return transactions;
 }
 
-getTable().then(table => {
-    // remove outlier 'Gross'
-    table.shift();
+function parseMorganStanleyReports(absolutePathToReportsDirectory) {
+    const getSummaryOfMonthlyReports = fs.readdirSync(absolutePathToReportsDirectory)
+        .filter(fileName => fileName.toLowerCase().endsWith('.pdf'))
+        .map(fileName => path.join(absolutePathToReportsDirectory, fileName))
+        .map(filePath => {
+            return getTable(filePath)
+                .then(table => {
+                    // remove outlier 'Gross'
+                    table.shift();
 
-    // normalize
-    let normalizedTable = table.map(row => row[0]);
+                    // normalize
+                    let normalizedTable = table.map(row => row[0]);
 
-    const transactions = extractTransactions(normalizedTable);
-    console.log(transactions);
+                    return {
+                        file: filePath,
+                        report: extractTransactions(normalizedTable)
+                    };
+                    // pprint(table);
+                });
+            });
 
-    // pprint(table);
-});
+    return Promise.all(getSummaryOfMonthlyReports);
+}
+
+parseMorganStanleyReports('C:\\Users\\adbosi\\Downloads\\tax documents 2018 final\\tax documents 2018\\morgan')
+    .then(reports => pprint(reports));
+
+module.exports = {
+    parseMorganStanleyReports
+};
