eb0a0d38ca1ce975aefb0f997949e7394b2333df
Denis Postu <depostu@microsoft.com>
2019-07-24 15:05:44 +0200

    UI polish for index.html

--- public/ui.js
+++ public/ui.js
@@ -1,82 +1,92 @@
 const UI = {};
 
-UI.setupIndexPage = () => {    
-    const appDiv = document.getElementById("app");
-    const form = document.getElementById("form");
+UI.setupIndexPage = () => {
+  const appDiv = document.getElementById("app");
+  const form = document.getElementById("form");
+  const inputForManualSelect = document.getElementById("input-manualSelect");
 
-    const fileLists = [];
+  const fileLists = [];
 
-    document.addEventListener("dragover", e => {
+  document.getElementById("dropzone").addEventListener("click", e => {
+    e.preventDefault();
+    inputForManualSelect.click();
+  });
+
+  inputForManualSelect.addEventListener("change", () => {
+    form.submit();
+  });
+
+  document.addEventListener("dragover", e => {
     e.preventDefault();
     e.dataTransfer.dropEffect = "copy";
 
     appDiv.classList.add("hovered");
-    });
+  });
 
-    document.addEventListener("dragleave", async e => {
+  document.addEventListener("dragleave", async e => {
     e.preventDefault();
     appDiv.classList.remove("hovered");
-    });
+  });
 
-    document.addEventListener("drop", async e => {
+  document.addEventListener("drop", async e => {
     e.preventDefault();
     appDiv.classList.remove("hovered");
 
     registerNewFileList(e.dataTransfer.files);
-    });
+  });
 
-    function registerNewFileList(fileList) {
-        fileLists.push(fileList);
+  function registerNewFileList(fileList) {
+    fileLists.push(fileList);
 
-        const hasPdfFiles = Array.from(fileList).some(f => f.type === 'application/pdf');
-        if (!hasPdfFiles) {
-            return;
-        }
-    
-        const input = document.createElement('input');
-        input.name = 'files';
-        input.type = 'file';
-        input.multiple = true;
-        input.files = fileList;
-
-        form.appendChild(input);
-        form.submit();
+    const hasPdfFiles = Array.from(fileList).some(
+      f => f.type === "application/pdf"
+    );
+    if (!hasPdfFiles) {
+      return;
     }
-}
+
+    const input = document.createElement("input");
+    input.name = "files";
+    input.type = "file";
+    input.multiple = true;
+    input.files = fileList;
+
+    form.appendChild(input);
+    form.submit();
+  }
+};
 
 UI.setupStatusPage = () => {
-    const preDiv = document.getElementById("pre");
-    const aDownload = document.getElementById("a-download");
-    const spanProgress = document.getElementById("span-progress");
+  const preDiv = document.getElementById("pre");
+  const aDownload = document.getElementById("a-download");
+  const spanProgress = document.getElementById("span-progress");
 
-    const updateInterval = setInterval(() => {
-        fetch(location.href + '/json')
-            .then(r => {
-                if (!r.ok) {
-                    preDiv.innerText = 'REPORT NO LONGER AVAILABLE';
-                    clearInterval(updateInterval);
+  const updateInterval = setInterval(() => {
+    fetch(location.href + "/json")
+      .then(r => {
+        if (!r.ok) {
+          preDiv.innerText = "REPORT NO LONGER AVAILABLE";
+          clearInterval(updateInterval);
 
-                    throw Error(r.statusText);
-                }
+          throw Error(r.statusText);
+        }
 
-                return r.json();
-            })
-            .then(json => {
-                if (json.status.aggregate === 'done') {
-                    clearTimeout(updateInterval);
+        return r.json();
+      })
+      .then(json => {
+        if (json.status.aggregate === "done") {
+          clearTimeout(updateInterval);
 
-                    aDownload.href = location.href + '/xlsx';
-                    aDownload.click();
-                }
-                if (json.status.aggregate === 'failed') {
-                    clearTimeout(updateInterval);
-                }
+          aDownload.href = location.href + "/xlsx";
+          aDownload.click();
+        }
+        if (json.status.aggregate === "failed") {
+          clearTimeout(updateInterval);
+        }
 
-                spanProgress.innerHTML = `Status: ${json.status.aggregate}`
-                preDiv.innerText = JSON.stringify(json, null, 2);
-            })
-            .catch(() => {
-
-            }); 
-    }, 500);
-}
+        spanProgress.innerHTML = `Status: ${json.status.aggregate}`;
+        preDiv.innerText = JSON.stringify(json, null, 2);
+      })
+      .catch(() => {});
+  }, 500);
+};
